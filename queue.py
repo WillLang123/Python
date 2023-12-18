120fcc146ba873de66a7b24ff7fad04735aa35d0
WillLang123 <williebear112003@gmail.com>
2023-12-18 07:43:48 -0600

    Added more files

---
+++ queue.py
@@ -0,0 +1,36 @@
+﻿class QueueError(TypeError):
+    pass
+    # Choose base class for the new exception.
+    #
+    #  Write code here
+    #
+class Queue:
+    def __init__(self):
+        self.__q = []
+        # Write code here
+        #
+
+    def put(self, elem):
+        self.__q.append(elem)
+        #
+        # Write code here
+        #
+
+    def get(self):
+        val = self.__q[0]
+        del self.__q[0]
+        return val
+        #
+        # Write code here
+        #
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
---
+++ timer.py
@@ -0,0 +1,55 @@
+﻿class Timer:
+    list = [0,0,0]
+    
+    def __init__(self,hr,min,sec):
+        self.list = [hr,min,sec]
+        # Write code here
+        #
+
+    def __str__(self):
+        word = ""
+        word+=str(self.list[0])
+        word+=":"
+        word+=str(self.list[1])
+        word+=":"
+        word+=str(self.list[2])
+        return word
+        #
+        # Write code here
+        #
+
+    def next_second(self):
+        self.list[2]+=1
+        if(self.list[2]>=60):
+            self.list[1]+=1
+            self.list[2]-=60
+            if(self.list[1]>=60):
+                self.list[0]+=1
+                self.list[1]-=60
+                if(self.list[0]>=24):
+                    self.list[0]-=24
+        #
+        # Write code here
+        #
+
+    def prev_second(self):
+        self.list[2]-=1
+        if(self.list[2]<0):
+            self.list[1]-=1
+            self.list[2]+=60
+            if(self.list[1]<0):
+                self.list[0]-=1
+                self.list[1]+=60
+                if(self.list[0]<0):
+                    self.list[0]+=24
+        #
+        # Write code here
+        #
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
